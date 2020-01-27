17a399c7e1b631237f69a8a8e28e9fc7a6e777c7
unknown <narges.rezaiean@gmail.com>
2020-01-27 19:27:25 +0330

    Updates

---
+++ precache-manifest.e2cdec90153ccb1a5d27c17f384b3f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f78b381d73e434f4ae802aa8587a7c1",
+    "url": "/narges-resume/index.html"
+  },
+  {
+    "revision": "ea3a00deea4d17d5355a",
+    "url": "/narges-resume/static/css/main.e41226cc.chunk.css"
+  },
+  {
+    "revision": "4e8ffd208ffeb6b18c89",
+    "url": "/narges-resume/static/js/2.0df408a4.chunk.js"
+  },
+  {
+    "revision": "d9bc9aff9b81d31e8e002b5479cfe228",
+    "url": "/narges-resume/static/js/2.0df408a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea3a00deea4d17d5355a",
+    "url": "/narges-resume/static/js/main.e586cca8.chunk.js"
+  },
+  {
+    "revision": "93b3055ebe77557b8b6b",
+    "url": "/narges-resume/static/js/runtime-main.e8980bdf.js"
+  }
+]);
